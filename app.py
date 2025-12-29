bd7cea849e5e5a1970760745b866c00b66797bb8
Ahmad Zulfadli <ahmadzulfadli342@gmail.com>
2025-12-29 12:35:34 +0700

    fix issue 502

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict():
 
 if __name__ == '__main__':
     # Gunakan debug=True saat pengembangan agar error terlihat jelas
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
