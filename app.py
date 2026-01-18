8612b3b61f8207f748fac5456a100281332161c8
ahmadzulfadli <52275966+ahmadzulfadli@users.noreply.github.com>
2026-01-18 12:51:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,5 @@ def chat_api():
 # 3. MENJALANKAN SERVER
 # ==========================================
 if __name__ == '__main__':
-    print("🚀 Server Flask Faraidh berjalan...")
-    print("👉 Buka di browser: http://127.0.0.1:5000")
     # debug=True agar server auto-restart kalau ada perubahan kode
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
